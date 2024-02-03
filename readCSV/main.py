18cd9766bc85c26a130bbc02e348ee859eebd044
Neicer Vásquez <NeicerYEANYT18@gmail.com>
2024-02-03 15:25:41 -0500

    New directory readCSV

---
+++ readCSV/main.py
@@ -0,0 +1,23 @@
+import utils
+import read_csv as rc
+import charts as gc
+
+def run():
+  data = rc.read_csv('data.csv')
+  data = list(filter(lambda item: item['Continent'] == 'Europe', data))
+
+  countries = list(map(lambda x: x['Country/Territory'], data))
+  percentages = list(map(lambda x: x['World Population Percentage'], data))
+  gc.generate_pie_chart(countries, percentages)
+
+# Para graficar en barras el país y su población
+  country = input('Type Country => ')
+  result = utils.population_by_country(data, country)
+  if len(result) > 0:
+    country = result[0]
+    labels, values = utils.get_population(country)
+    gc.generate_bar_chart(country['Country/Territory'], labels, values)
+
+
+if __name__ == '__main__':
+  run()
---
+++ readCSV/read_csv.py
@@ -0,0 +1,21 @@
+import csv
+
+def read_csv(ruta):
+  with open(ruta, 'r') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    main_row = next(reader)
+    world_population = []
+    for rows in reader:
+      data = dict(zip(main_row, rows))
+      world_population.append(data)
+    return world_population
+
+
+# def delimitar_pais_rango(data):
+#   rango_pais = list(filter(lambda key: int(key['Rank']) >= 234, data))
+#   print(rango_pais)
+#   print(len(rango_pais))
+
+if __name__ == '__main__':
+  world_data = read_csv('data.csv')
+  # delimitar_pais_rango(world_data)
